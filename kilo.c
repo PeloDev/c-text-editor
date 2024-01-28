9fcbaf951c66aea8af33b7384020ca2ce94cd0b6
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-01-28 13:27:12 +0200

    (raw mode): revert raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,5 +1,15 @@
+# include <stdlib.h>
 # include <termios.h>
 # include <unistd.h>
+
+struct termios orig_termios;
+
+/**
+ * Restore original terminal attributes
+*/
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 /**
  * Set the terminals attributes so that it behaves more like a Text Editor by:
@@ -9,12 +19,15 @@ void enableRawMode() {
     /**
      * Turn off echoing - you won't see your input in the terminal anymore.
      * Disabling ECHO is how password inputs are protected in the terminal (e.g. `sudo` password input).
-     * At this point "q to quit" won't set your terminal back to ECHO inputs, 
-     * Ctrl+C will reset your terminal though (this is true for most cases)
+     * Note: "q to quit" won't set your terminal back to ECHO inputs unless we revert terminal attributes back
+     * to their original values. (If all else fails Ctrl+C will reset your terminal in most cases) 
     */
-    struct termios raw;
-    // get terminal attributes and assign them to the raw termios struct
-    tcgetattr(STDIN_FILENO, &raw);
+    // get terminal attributes and assign them to the original termios struct
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    // revert to original at exit
+    atexit(disableRawMode);
+    // create raw struct from original for updated terminal attributes
+    struct termios raw = orig_termios;
     // update the terminal attributes in your struct to disable (`~` is bitwsie-NOT operator) the ECHO flag
     raw.c_lflag &= ~(ECHO);
     // set the terminal attributes with the new value (TCSAFLUSH -> apply this after all pending outputs are written to the terminal, and discard unread inputs)
