e75831fad27ddabaad6fc0c2c6276d0dc3cfe6f0
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-01-28 12:57:37 +0200

    read keyboard inputs in "cooked mode"

--- kilo.c
+++ kilo.c
@@ -1,3 +1,13 @@
+# include <unistd.h>
+
 int main() {
+    /**
+     * Listen for key press events from the user.
+     * Read 1 byte from the standard input into char c until the end of the standard input (file)?
+     * Keyboard inputs will get read into char c in "canonical/cooked mode" 
+     * (ie: they will only be read after pressing ENTER, like terminal commands)
+    */
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1);
     return 0;
 }
