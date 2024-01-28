4e7879deb3fd85ae51a24bdcca1a4f32e80e2865
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-01-28 12:41:27 +0200

    init

---
+++ compileAndRun.zsh
@@ -0,0 +1,3 @@
+# Run this command to allow permissions to run this script: `chmod u+x ./compileAndRun.zsh` 
+make # see Makefile - compiles the program, warning for bad code
+./kilo # runs the code
