1eaa09e760f4613d7350e82c852589765482e0be
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-01-28 13:34:03 +0200

    (raw mode): turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -14,22 +14,31 @@ void disableRawMode() {
 /**
  * Set the terminals attributes so that it behaves more like a Text Editor by:
  * - Turning off echoing
+ * - Turning off canonical mode
 */
 void enableRawMode() {
-    /**
-     * Turn off echoing - you won't see your input in the terminal anymore.
-     * Disabling ECHO is how password inputs are protected in the terminal (e.g. `sudo` password input).
-     * Note: "q to quit" won't set your terminal back to ECHO inputs unless we revert terminal attributes back
-     * to their original values. (If all else fails Ctrl+C will reset your terminal in most cases) 
-    */
     // get terminal attributes and assign them to the original termios struct
     tcgetattr(STDIN_FILENO, &orig_termios);
+    
     // revert to original at exit
     atexit(disableRawMode);
+
     // create raw struct from original for updated terminal attributes
     struct termios raw = orig_termios;
-    // update the terminal attributes in your struct to disable (`~` is bitwsie-NOT operator) the ECHO flag
-    raw.c_lflag &= ~(ECHO);
+
+    /**
+     * Turn off echoing (ECHO)
+     * - You won't see your input in the terminal anymore.
+     * - Disabling the ECHO flag is how password inputs are protected in the terminal (e.g. `sudo` password input).
+     * - Note: "q to quit" won't set your terminal back to ECHO inputs unless we revert terminal attributes back
+     * to their original values. (If all else fails Ctrl+C will reset your terminal in most cases) 
+     *
+     * Turn off canonical mode (ICANON)
+     * - Inputs will be read byte-by-byte instead of after pressing ENTER.
+     * - "q to quit" will enact immediately
+    */
+    raw.c_lflag &= ~(ECHO | ICANON); // `~` is bitwise NOT operator
+
     // set the terminal attributes with the new value (TCSAFLUSH -> apply this after all pending outputs are written to the terminal, and discard unread inputs)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
